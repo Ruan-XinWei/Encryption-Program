2b685562e498b532d7aaeeeade6579c2d64fd2fa
Ruan-xinwei <ruanxinwei.2001@gmail.com>
2020-12-17 00:18:37 +0800

    添加项目文件。

---
+++ RSA/RSA.h
@@ -0,0 +1,32 @@
+﻿
+// RSA.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CRSAApp:
+// 有关此类的实现，请参阅 RSA.cpp
+//
+
+class CRSAApp : public CWinApp
+{
+public:
+	CRSAApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CRSAApp theApp;
